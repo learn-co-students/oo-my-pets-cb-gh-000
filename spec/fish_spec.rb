4275bc35fa11d4749fe5b5301ebc332667852a55
Katie Hoffman <ktahoffman@gmail.com>
2014-06-18 15:17:26 -0400

    adds cat dog and fish specs

--- spec/fish_spec.rb
+++ spec/fish_spec.rb
@@ -1,1 +1,28 @@
 require_relative 'spec_helper.rb'
+
+describe Fish do
+
+  let(:fish) { Fish.new("Nemo") }
+
+  it "can initialize a fish" do
+    expect(fish).to be_a(Fish)
+  end
+
+  it "initializes with a name" do
+    expect(fish.name).to eq("Nemo")
+  end
+
+  it "can't change its name" do 
+    expect(fish.name = "Goldy").to raise_error
+  end
+
+  it "initializes with a nervous mood" do
+    expect(fish.mood).to eq("nervous")
+  end
+
+  it "can change its mood" do 
+    fish.mood = "standoffish"
+    expect(fish.mood).to eq("standoffish")
+  end
+  
+end
