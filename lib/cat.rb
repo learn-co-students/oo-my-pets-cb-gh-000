2769a0296336133ec8eb84b3997a46522c3170d3
Katie Hoffman <ktahoffman@gmail.com>
2014-06-18 16:17:36 -0400

    removes answer

--- lib/cat.rb
+++
@@ -1,8 +0,0 @@
-class Cat
-  attr_reader :name
-  attr_accessor :mood
-  def initialize(name)
-    @name = name
-    @mood = "nervous"
-  end
-end
--- lib/dog.rb
+++
@@ -1,8 +0,0 @@
-class Dog
-  attr_reader :name
-  attr_accessor :mood
-  def initialize(name)
-    @name = name
-    @mood = "nervous"
-  end
-end
