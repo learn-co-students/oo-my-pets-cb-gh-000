ad4c732512b1bffbea5618fe866d1a9d53e41c20
7s Dream <7seconddream@gmail.com>
2017-02-01 09:26:51 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,80 @@
+require_relative "cat"
+require_relative "dog"
+require_relative "fish"
+
 class Owner
-  # code goes here
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.size
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  attr_accessor :name, :pets
+  attr_reader :species
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @pets = {:fishes => [], :cats => [], :dogs => []}
+    @@all << self
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def buy_fish(name)
+    fish = Fish.new(name)
+    @pets[:fishes] << fish
+  end
+
+  def buy_cat(name)
+    cat = Cat.new(name)
+    @pets[:cats] << cat
+  end
+
+  def buy_dog(name)
+    dog = Dog.new(name)
+    @pets[:dogs] << dog
+  end
+
+  def walk_dogs
+    @pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def play_with_cats
+    @pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    @pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    @pets.each do |key, some_pets|
+      some_pets.each do |pet|
+        pet.mood = "nervous"
+      end
+      some_pets.clear
+    end
+  end
+
+  def list_pets
+    "I have #{@pets[:fishes].size} fish, #{@pets[:dogs].size} dog(s), and #{@pets[:cats].size} cat(s)."
+  end
+
 end
