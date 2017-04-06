04d5345ad71a7658682df20d5a34ab99eb15076e
Federico Gobbo <federico.gobbo.2@studenti.unipd.it>
2017-04-06 17:22:52 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,13 @@
 class Cat
-  # code goes here
+  attr_accessor :mood
+
+  def initialize(name, mood='nervous')
+    @name = name
+    @mood = mood
+  end
+
+  def name
+    @name.dup.freeze
+  end
+
 end
