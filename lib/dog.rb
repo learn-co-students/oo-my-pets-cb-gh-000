cb6267a60a073433488a6efaca92c917e6e2640b
Leslie Huang <lesliehuang@gmail.com>
2017-07-04 18:13:37 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name
+
+  def initialize(name, mood = "nervous")
+    @name = name
+    @mood = mood
+  end
+
+  def mood=(mood)
+    @mood = mood
+  end
+
 end
