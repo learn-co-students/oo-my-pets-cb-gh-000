23ee4c7b0416c0c61073dc8e9498d0abd4ea94b2
Brennan Brown <brennan.brown@edu.sait.ca>
2017-08-22 22:42:41 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,9 @@
 class Cat
-  # code goes here
+  # Method initializes the instance variables needed for the class to function.
+  attr_accessor :mood
+  attr_reader :name
+  def initialize(name, mood = "nervous")
+    @name = name
+    @mood = mood
+  end
 end
--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,9 @@
 class Fish
-  # code goes here
+  # Method initializes the instance variables needed for the class to function.
+  attr_accessor :mood
+  attr_reader :name
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
 end
