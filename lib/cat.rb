dbd3c5cc2ae75babf044bed9e3a8d635ff141cff
Yi Xuan Sim <yxuan1996@gmail.com>
2017-09-06 18:58:34 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,8 @@
 class Cat
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,8 @@
 class Dog
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
 end
