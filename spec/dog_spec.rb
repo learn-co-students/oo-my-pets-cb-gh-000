b05b6bf074976ab7629d73a437cb5755cfee3418
Jason Decker <curiositypaths@users.noreply.github.com>
2017-11-27 14:06:52 -0500

    Specify what error should be raised when calling an undefined setter method

--- spec/dog_spec.rb
+++ spec/dog_spec.rb
@@ -12,17 +12,17 @@ describe Dog do
     expect(dog.name).to eq("Daisy")
   end
 
-  it "can't change its name" do 
-    expect { dog.name = "Fido" }.to raise_error
+  it "can't change its name" do
+    expect { dog.name = "Fido" }.to raise_error NoMethodError
   end
 
   it "initializes with a nervous mood" do
     expect(dog.mood).to eq("nervous")
   end
 
-  it "can change its mood" do 
+  it "can change its mood" do
     dog.mood = "hungry"
     expect(dog.mood).to eq("hungry")
   end
-  
+
 end
