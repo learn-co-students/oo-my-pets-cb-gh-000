2e9f138218212afb5541f954593f86666262c7c4
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-21 01:59:19 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
-  # code goes here
+  
+  attr_accessor :mood
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+  
 end
+
+
+
+# learn spec/dog_spec.rb --fail-fast
