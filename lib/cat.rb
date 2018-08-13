88538b86900869610a9b80f8c186fc5e99ae5890
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-13 11:12:03 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,9 @@
 class Cat
+  attr_reader :name 
+  attr_accessor :mood
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
   # code goes here
 end
--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,10 @@
 class Fish
+  attr_reader :name
+  attr_accessor :mood
+  
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
   # code goes here
 end
