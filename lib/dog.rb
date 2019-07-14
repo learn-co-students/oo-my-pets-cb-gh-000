a45d26116ef68b750fe3482afb06e251ce93b412
Danish Naseem <danishnaseem05@gmail.com>
2019-07-14 05:40:32 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,30 @@
 class Dog
   # code goes here
+  @@all = []
+
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  def initialize(name, owner)
+    @name = name
+    @mood = "nervous"
+    self.add_owner(owner)
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_owner(owner)
+    if self.owner != owner
+      self.owner = owner
+      owner.add_dog(self)
+    end
+  end
+
+  def remove_owner
+    self.owner = nil
+  end
 end
