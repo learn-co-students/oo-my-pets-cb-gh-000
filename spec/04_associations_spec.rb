3e0198977f1a645a92a42bcce5474e546614f446
Donnadieu <donnadieu.86@gmail.com>
2019-07-17 19:57:26 -0400

    Refactored tests

--- spec/04_associations_spec.rb
+++ spec/04_associations_spec.rb
@@ -7,71 +7,83 @@ describe "Associations" do
 
   after do
     Owner.all.clear
-    @owner.cats.clear
-    @owner.dogs.clear
   end
-  context "Owner" do
+
+  context 'Owner instance methods' do
     describe "#cats" do
-      it "initializes with a cats array" do
-        expect(@owner.cats).to be_a(Array)
+      it "returnsa a collection of all the cats that belong to the owner" do
+        cat_1 = Cat.new("Garfield", @owner)
+        cat_2 = Cat.new("Fido", @owner)
+        cat_3 = Cat.new("Whiskers", @owner)
+
+        expect(@owner.cats[0]).to eq(cat_1)
+        expect(@owner.cats[1]).to eq(cat_2)
+        expect(@owner.cats[2]).to eq(cat_3)
+        expect(@owner.cats.count).to eq(3)
       end
     end
 
     describe "#dogs" do
-      it "initializes with a dogs array" do
-        expect(@owner.dogs).to be_a(Array)
+      it "returnsa a collection of all the dogs that belong to the owner" do
+        dog_1 = Dog.new("Fido", @owner)
+        dog_2 = Dog.new("Snuffles", @owner)
+        dog_3 = Dog.new("Charley", @owner)
+
+        expect(@owner.dogs[0]).to eq(dog_1)
+        expect(@owner.dogs[1]).to eq(dog_2)
+        expect(@owner.dogs[2]).to eq(dog_3)
+        expect(@owner.dogs.count).to eq(3)
       end
     end
-  end
-
-  context "Cat" do
-    describe "#initialize" do
-      it "it adds the cat to the Owner's collection of cats" do
-        cat = Cat.new("Garfield", @owner)
-        expect(@owner.cats).to include(cat)
-      end
-    end
-  end
-
-  context "Dog" do
-    describe "#initialize" do
-      it "it adds the dog to the Owner's collection of dogs" do
-        dog = Dog.new("Daisy", @owner)
-        expect(@owner.dogs).to include(dog)
-      end
-    end
-  end
-
-  context 'Owner instance methods' do
     describe "#buy_cat" do
       it 'can buy a cat that is an instance of the Cat class' do
         expect(@owner.cats.count).to eq(0)
+
         @owner.buy_cat("Crookshanks")
+        @owner.buy_cat("Whiskers")
+        @owner.buy_cat("Garfield")
+
         @owner.cats.each do |cat|
           expect(cat).to be_a(Cat)
         end
-        expect(@owner.cats.count).to eq(1)
+
+        expect(@owner.cats.count).to eq(3)
       end
 
       it 'knows about its cats' do
         @owner.buy_cat("Crookshanks")
+        @owner.buy_cat("Whiskers")
+        @owner.buy_cat("Garfield")
+
         expect(@owner.cats[0].name).to eq("Crookshanks")
+        expect(@owner.cats[1].name).to eq("Whiskers")
+        expect(@owner.cats[2].name).to eq("Garfield")
       end
     end
 
     describe "#buy_dog" do
       it 'can buy a dog that is an instance of the Dog class' do
         expect(@owner.dogs.count).to eq(0)
+
         @owner.buy_dog("Snuffles")
+        @owner.buy_dog("Fido")
+        @owner.buy_dog("Rover")
+
         @owner.dogs.each do |dog|
           expect(dog).to be_a(Dog)
         end
-        expect(@owner.dogs.count).to eq(1)
+
+        expect(@owner.dogs.count).to eq(3)
       end
 
       it 'knows about its dogs' do
         @owner.buy_dog("Snuffles")
+        @owner.buy_dog("Fido")
+        @owner.buy_dog("Rover")
+
         expect(@owner.dogs[0].name).to eq("Snuffles")
+        expect(@owner.dogs[1].name).to eq("Fido")
+        expect(@owner.dogs[2].name).to eq("Rover")
       end
     end
 
@@ -120,7 +132,7 @@ describe "Associations" do
     describe "#list_pets" do
       it 'can list off its pets' do
         @owner.buy_cat("Crookshanks")
-        @owner.buy_dog("Fido")
+        @owner.buy_cat("Fido")
         @owner.buy_dog("Snuffles")
         @owner.buy_dog("Charley")
         expect(@owner.list_pets).to eq("I have #{@owner.dogs.count} dog(s), and #{@owner.cats.count} cat(s).")
