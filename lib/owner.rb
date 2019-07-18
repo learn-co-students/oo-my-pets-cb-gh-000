3778c8dd28c145e64699d38785a18efe27ae7795
Donnadieu <donnadieu.86@gmail.com>
2019-07-17 20:00:37 -0400

    Changed solution to match new tests

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,75 +1,65 @@
 class Owner
+  # code goes here
+  attr_reader :name, :species
 
-    attr_reader :species, :name
+  @@all = []
 
-    @@all = []
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+  end
 
-    def initialize(name)
-        @name = name
-        @species = "human"
-        @@all << self
+  def say_species
+    "I am a #{self.species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    self.all.count
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def cats
+    Cat.all.select{|cat| cat.owner == self}
+  end
+
+  def dogs
+    Dog.all.select{|dog| dog.owner == self}
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def walk_dogs
+    self.dogs.each { |dog| dog.mood = "happy" }
+  end
+
+  def feed_cats
+    self.cats.each { |cat| cat.mood = "happy" }
+  end
+
+  def sell_pets
+    pets = self.dogs + self.cats
+
+    pets.each do |pet|
+      pet.mood = "nervous"
+      pet.owner = nil
     end
+  end
 
-    def self.all 
-      @@all 
-    end
-
-    def self.count
-        self.all.length
-    end
-
-    def say_species
-        "I am a #{self.species}."
-    end
-
-    def buy_cat(name)
-        Cat.new(name, self)
-    end
-
-    def buy_dog(name)
-        Dog.new(name, self)
-    end
-
-    def cats
-        Cat.all.select do |cat|
-            cat.owner == self
-        end
-    end
-
-    def dogs
-        Dog.all.select do |dog|
-            dog.owner == self
-        end
-    end
-
-    def walk_dogs
-        self.dogs.each {|dog| dog.mood = "happy"}
-    end
-
-    def feed_cats
-        self.cats.each {|cat| cat.mood = "happy"}
-    end
-
-    def list_pets
-        number_of_dogs = self.dogs.count
-        number_of_cats = self.cats.count
-        return "I have #{number_of_dogs} dog(s), and #{number_of_cats} cat(s)."
-    end
-
-    def sell_pets
-        all_pets = self.cats + self.dogs
-        all_pets.each do |pet|
-            sell_pet(pet)
-        end
-    end
-
-    def sell_pet(pet)
-        pet.owner = nil
-        pet.mood = "nervous"
-    end
-
-    def self.reset_all
-        self.all.clear
-    end
-
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
 end
