91f28f4e6ceefcc3581a27c5da652e0d55532a70
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-14 15:58:26 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,28 @@
 class Owner
-  # code goes here
+
+    attr_reader :name, :species
+
+    @@all = []
+
+    def initialize(name)
+      @name = name
+      @species = "human"
+      @@all << self
+    end
+
+    def say_species
+      "I am a #{@species}."
+    end
+
+    def self.all
+      @@all
+    end
+
+    def self.count
+      @@all.size
+    end
+
+    def self.reset_all
+      @@all.clear
+    end
 end
