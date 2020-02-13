387b75b22f07311002024c4bb1ada1460f65e813
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-13 19:01:36 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
-  # code goes here
+  attr_accessor :owner ,  :mood
+  attr_reader :name
+  @@all =[]
+  def initialize(name , owner )
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+  def self.count
+    @@all.size
+  end
+
+  def self.find_or_create_a_dog(name , owner  )
+    # dog = @@all.find {|c| c.name == name  }
+    # dog.owner = owner if dog
+    self.new(name , owner )
+  end
 end
